0495a6fc889b4a8ef081b63f0117b72c163e0c9f
Leonardo Hobeni <leonardohobeni@gmail.com>
2025-12-16 12:07:59 +0200

    Created Topic and Entry Models

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Topic(models.Model):
+    """ This class simulates the entry topic """
+    
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+    
+
+class Entry(models.Model):
+    """ This class simulates the entry content """
+
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Entries'
+    
+    def __str__(self):
+        if len(self.text) > 50:
+            return self.text[:50] + '...'
+        return self.text
