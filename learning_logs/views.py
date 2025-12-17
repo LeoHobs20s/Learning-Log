b7c306d74be468a37eb7932565166ca2032b13fb
Leonardo Hobeni <leonardohobeni@gmail.com>
2025-12-17 20:54:23 +0200

    Added More Pages

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 def index(request):
+    """ This view represents the Home page """
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """ This view represents the Topics page """
+
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics':topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """ This view represent the Topic Page """
+
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic':topic, 'entries':entries}
+    return render(request, 'learning_logs/topic.html', context)
