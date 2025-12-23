396395a0380fc679af002e10c28a8540c8ed791d
Leonardo Hobeni <leonardohobeni@gmail.com>
2025-12-23 17:41:41 +0200

    Added User registration Page System

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+
 from .forms import LoginForm
 
 
@@ -12,20 +14,47 @@ def login_view(request):
         form = LoginForm()
     else:
         # POST Data Submitted; process credentials
-        form = LoginForm(request.POST)
+        form = LoginForm(data=request.POST)
         if form.is_valid():
             username = request.POST['username']
             password = request.POST['password']
+            # validate user credentials
             user = authenticate(request, username=username, password=password)
             if user is not None:
+                # create a session for the current user
                 login(request, user)
                 messages.success(request, f'Welcome, {user.username}')
-                return redirect('index')
+                return redirect('index') # sending user to home page
             else:
                 messages.error(request, 'Invalid credentials')
     context = {'form':form}
     return render(request, 'users/login.html', context)
 
+
 def logout_view(request):
+    """ Logout the current user in session """
+
+    # remove information of the current user from the session
     logout(request)
     return redirect('login_view')
+
+
+def register(request):
+    """ Registering new user in the site """
+
+    if request.method != 'POST':
+        # No data submitted; create a blank form
+        form = UserCreationForm()
+    
+    else:
+        # POST Data Submitted; process the user's information
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            # authenticate and login user
+            user = authenticate(username=new_user.username, password=request.POST['password1'])
+            login(request, user)
+            return redirect('index') # sending user to home page
+    
+    context = {'form':form}
+    return render(request, 'users/register.html', context)
