07b523eabec1e2370a471c7c63b9199b891ff8d0
Leonardo Hobeni <leonardohobeni@gmail.com>
2025-12-29 16:43:30 +0200

    Restricted Pages to logged in user, linked topics to owner and entries

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, Http404
 from django.urls  import reverse
 
 from .models import Topic, Entry
@@ -9,21 +10,29 @@ def index(request):
     """ This view represents the Home page """
     return render(request, 'learning_logs/index.html')
 
+
+@login_required
 def topics(request):
     """ This view represents the Topics page """
 
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added') 
     context = {'topics':topics}
     return render(request, 'learning_logs/topics.html', context)
 
+
+@login_required
 def topic(request, topic_id):
     """ This view represent the Topic Page """
 
     topic = get_object_or_404(Topic, pk=topic_id)
     entries = topic.entry_set.order_by('-date_added')
+    if topic.owner != request.user:
+        raise Http404
     context = {'topic':topic, 'entries':entries}
     return render(request, 'learning_logs/topic.html', context)
 
+
+@login_required
 def new_topic(request):
     """ This view will generate code for the new topic form """
 
@@ -34,12 +43,16 @@ def new_topic(request):
         # Data submitted; process data
         form = TopicForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return HttpResponseRedirect(reverse('topics'))
     
     context = {'form':form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+
+@login_required
 def new_entry(request, topic_id):
     """ Adding new entry data """
 
@@ -59,11 +72,16 @@ def new_entry(request, topic_id):
     context = {'topic':topic, 'form':form}
     return render(request, 'learning_logs/new_entry.html', context)
 
+
+@login_required
 def edit_entry(request, entry_id):
     """ Edit existing entries """
 
     entry = get_object_or_404(Entry, pk=entry_id)
     topic = entry.topic
+
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         # No Data Submitted; create blank form 
